0c1503bbffd1e386587a1f282485dc9a04c74c36
vnotes <vnotes@protonmail.com>
2019-06-02 14:41:04 +0800

    format code

--- oauth_app.py
+++ oauth_app.py
@@ -1,5 +1,3 @@
-import json
-
 import aiohttp
 from sanic import Sanic
 from sanic import response
@@ -9,7 +7,8 @@ app = Sanic()
 
 @app.route('/api/login', methods=frozenset({"GET"}))
 async def login(request):
-    return response.redirect("https://github.com/login/oauth/authorize?client_id=910b187636bfb3f33ac9&redirect_uri=http://127.0.0.1:8000/oauth/redirect")
+    return response.redirect(
+        "https://github.com/login/oauth/authorize?client_id=910b187636bfb3f33ac9&redirect_uri=http://127.0.0.1:8000/oauth/redirect")
 
 
 @app.route("/oauth/redirect")
@@ -20,10 +19,10 @@ async def oauth_code(request):
         async with session.post(token_url, json={"client_id": "910b187636bfb3f33ac9",
                                                  "client_secret": "b426ca8d5a34cdc744566c1d6a331d44efbb41dd",
                                                  "code": code}) as resp:
-
             token = await resp.text()
             rsp_json = {each.split('=')[0]: each.split('=')[1] for each in token.split('&')}
             return response.json(rsp_json)
+
 
 if __name__ == "__main__":
     app.run()
